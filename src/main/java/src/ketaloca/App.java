d1ba48bf4cfee1fd88649cc4dfb294ffbbb15062
ketaloca <xesuspb@gmail.com>
2024-11-24 00:31:53 +0100

    Dependencia añadida, devolviendo json correctamente

--- src/main/java/src/ketaloca/App.java
+++ src/main/java/src/ketaloca/App.java
@@ -2,9 +2,15 @@ package src.ketaloca;
 
 import io.javalin.Javalin;
 
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(4000);
-        app.get("/", ctx -> ctx.result("Hello world!!"));
+        app.get("/", ctx -> ctx.json(
+                Map.of(
+                        "message", "Hello world!",
+                        "message2", "Hola mundo!")
+        ));
     }
 }
