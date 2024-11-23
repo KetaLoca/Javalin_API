e0698cde190a2c3c031b08f8ef56b290445c6c8a
ketaloca <xesuspb@gmail.com>
2024-11-24 00:08:27 +0100

    Primer endpoint de prueba funcionando

--- src/main/java/src/ketaloca/App.java
+++ src/main/java/src/ketaloca/App.java
@@ -5,5 +5,6 @@ import io.javalin.Javalin;
 public class App {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(4000);
+        app.get("/", ctx -> ctx.result("Hello world!!"));
     }
 }
