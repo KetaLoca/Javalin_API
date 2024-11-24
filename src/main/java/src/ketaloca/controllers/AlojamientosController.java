ac7eedfcae988e25305bbd2319edd2e6c775324e
ketaloca <xesuspb@gmail.com>
2024-11-24 21:26:42 +0100

    Avanzando lógica AlojamientosController

--- src/main/java/src/ketaloca/controllers/AlojamientosController.java
+++ src/main/java/src/ketaloca/controllers/AlojamientosController.java
@@ -2,15 +2,41 @@ package src.ketaloca.controllers;
 
 import io.javalin.http.Context;
 import src.ketaloca.models.Alojamiento;
+import src.ketaloca.repositories.AlojamientosRepository;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class AlojamientosController {
 
     public static void getAlojamientos(Context ctx) {
+        String email = ctx.queryParam("userEmail");
+        if (email == null || email.isEmpty()) {
+            ArrayList<Alojamiento> alojamientos = AlojamientosRepository.getAll();
+            if (alojamientos != null) {
+                ctx.status(200).json(alojamientos);
+                return;
+            }
+            ctx.status(404).json(Map.of("message", "Not found"));
+            return;
+        }
+
+        ArrayList<Alojamiento> alojamientos = AlojamientosRepository.getByEmail(email);
+        if (alojamientos != null) {
+            ctx.status(200).json(alojamientos);
+            return;
+        }
+        ctx.status(404).json(Map.of("message", "Not found"));
     }
 
     public static void getAlojamientoById(Context ctx) {
+        String id = ctx.pathParam("id");
+        Alojamiento alojamiento = AlojamientosRepository.getById(id);
+        if (alojamiento != null) {
+            ctx.status(200).json(alojamiento);
+            return;
+        }
+        ctx.status(404).json(Map.of("message", "Not found"));
     }
 
     public static void create(Context ctx) {
