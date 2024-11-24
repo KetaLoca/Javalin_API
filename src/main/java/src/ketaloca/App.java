8f7b37c50ffa85938b8f5080cf620f3feaaa5ed7
ketaloca <xesuspb@gmail.com>
2024-11-24 03:10:20 +0100

    Cors configurados en principio

--- src/main/java/src/ketaloca/App.java
+++ src/main/java/src/ketaloca/App.java
@@ -6,7 +6,17 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(4000);
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableCors(cors ->
+            {
+                cors.addRule(it -> {
+                    it.allowCredentials = true;
+                    it.allowHost("http://localhost:3000");
+                    it.exposeHeader("Content-Type");
+                });
+            });
+        }).start(4000);
+
         app.get("/", ctx -> ctx.json(
                 Map.of(
                         "message", "Hello world!",
