9fd0c857fce22f495fc3e8d8866e58d5783a85ef
ketaloca <xesuspb@gmail.com>
2024-11-24 16:05:22 +0100

    Modelado de clases de la capa models terminada

--- src/main/java/src/ketaloca/models/Reserva.java
+++ src/main/java/src/ketaloca/models/Reserva.java
@@ -1,4 +1,53 @@
 package src.ketaloca.models;
 
 public class Reserva {
+    private String id, userEmail, alojamientoId, fechaInicio, fechaFin;
+
+    public Reserva(String id, String userEmail, String alojamientoId, String fechaInicio, String fechaFin) {
+        this.id = id;
+        this.userEmail = userEmail;
+        this.alojamientoId = alojamientoId;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getAlojamientoId() {
+        return alojamientoId;
+    }
+
+    public void setAlojamientoId(String alojamientoId) {
+        this.alojamientoId = alojamientoId;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 }
--- src/main/java/src/ketaloca/models/User.java
+++ src/main/java/src/ketaloca/models/User.java
@@ -1,4 +1,54 @@
 package src.ketaloca.models;
 
 public class User {
+    private String email, password, nombre, apellidos;
+    private int telefono;
+
+    public User(String email, String password, String nombre, String apellidos, int telefono) {
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 }
